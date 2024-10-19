ad6efd8503e6058e58062003c520ab2b5a9c31c5
unknown <rejayflora@gmail.com>
2024-10-19 21:03:44 +0800

    .

--- resources/js/Components/AboutUs.vue
+++ resources/js/Components/AboutUs.vue
@@ -10,7 +10,6 @@
         />
         <div class="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-500"></div>
       </div>
-
       <!-- Text Section -->
       <div>
         <p class="text-secondary font-semibold tracking-wider uppercase">About Us</p>
@@ -20,34 +19,6 @@
         <p class="mt-4 text-sm md:text-base text-white">
           At Cross Digital, we are driven by a passion for innovation and excellence in the ever-evolving world of digital marketing. Our team crafts unique, data-driven strategies to help your business thrive in both online and traditional media landscapes.
         </p>
-        
-
-       <!-- Vision and Mission Grid -->
-       <!-- <div class="grid grid-cols-1 lg:grid-cols-2 gap-8 my-8 md:my-14">
-          <div class="p-6 sm:p-8 bg-blackGray rounded-lg shadow-md hover:shadow-xl transition-shadow">
-            <div>
-              <EyeIcon class="w-6 sm:w-8 text-secondary mb-3" />
-              <h5 class="text-xl sm:text-2xl mb-2 font-semibold tracking-tight text-white">
-                Our Vision
-              </h5>
-            </div>
-            <p class="mb-3 text-sm sm:text-base text-white">
-              To empower businesses with cutting-edge digital marketing solutions, leading them to sustained growth and unmatched market dominance in the competitive digital landscape.
-            </p>
-          </div>
-
-          <div class="p-6 sm:p-8 bg-blackGray rounded-lg shadow-md hover:shadow-xl transition-shadow">
-            <div>
-              <RocketLaunchIcon class="w-6 sm:w-8 text-secondary mb-3" />
-              <h5 class="text-xl sm:text-2xl mb-2 font-semibold tracking-tight text-white">
-                Our Mission
-              </h5>
-            </div>
-            <p class="mb-3 text-sm sm:text-base text-white">
-              To deliver strategic, data-driven marketing solutions that transform our clients' brands, enhance their online visibility, drive traffic, and maximize ROI across digital and traditional media spaces.
-            </p>
-          </div>
-        </div> -->
         <div class="rounded-lg text-darkGray shadow-lg p-6 md:p-8">
           <div class="flex justify-around mb-6">
             <button @click="activeTab = 'mission'" :class="activeTab === 'mission' ? 'border-b-2 border-secondary' : ''"class="uppercase font-semibold text-lg md:text-lg px-4 py-2 focus:outline-none">
@@ -72,7 +43,6 @@
             </p>
           </div>
         </div>
-        
         <!-- Meet the Team Button -->
         <div class="mt-4">
           <button
